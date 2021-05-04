2024e2b1e0f191d557b54f6141a2fb8f1fd72a1e
mebangit123 <mebannongrum333@gmail.com>
2021-05-04 15:48:21 +0530

    Added Welcome message

---
+++ FindMax/src/main/java/com/gen/findmax/utils/FindMax.java
@@ -0,0 +1,16 @@
+package com.gen.findmax.utils;
+
+public class FindMax {
+	public static Integer findMaxInt(Integer x, Integer y, Integer z) {
+		int max = x;
+		if(y.compareTo(max) > 0)
+			max = y;
+		if(z.compareTo(max) > 0)
+			max = z;
+		//printMax(x,y,z,max);
+		return max;
+	}
+	public static void main(String[] args) {
+		System.out.println("Find Maximum Problem Using Generics..");
+	}
+}
